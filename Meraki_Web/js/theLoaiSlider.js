65d4ad3d001b82e88e46d0d175645b93e70ae6e4
HP <giang26032001@gmail.com>
2021-12-13 20:05:17 +0700

    Responsive Theloai.html

--- Meraki_Web/js/theLoaiSlider.js
+++ Meraki_Web/js/theLoaiSlider.js
@@ -17,51 +17,19 @@ $(document).ready(function () {
                 breakpoint: 700,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 2,
                 }
             },
             {
-                breakpoint: 500,
+                breakpoint: 400,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
                 }
             }
         ]
     });
 });
 
-$(document).ready(function () {
-    $('.slider_second').slick({
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        infinite: true,
-        prevArrow: "<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow: "<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-        responsive: [
-            {
-                breakpoint: 1140,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 700,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-
-        ]
-    });
-});
 
 
