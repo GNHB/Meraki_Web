ea200e9eaaa0c87e9d9e6b73b11a9550ec6c4768
HP <giang26032001@gmail.com>
2021-12-16 09:56:38 +0700

    responsive GioHang

--- Meraki_Web/js/angular.js
+++
@@ -1,8 +0,0 @@
-﻿////var App = angular.module('myApp', []);
-
-////App.controller('myController', function ($scope, $http) {
-////    $http.get('book.json')
-////        .then(function (res) {
-////            $scope.theloai = res.data;
-////        });
-////});
